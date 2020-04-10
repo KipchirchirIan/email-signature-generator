60c630694e2501e5779bac712bdfaa83ef5bcb05
KipchirchirIan <potterke4@gmail.com>
2020-04-10 07:59:32 +0300

    Add hardcoded sample template with resources

---
+++ templates/template01.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template 01</title>
+</head>
+
+<body>
+
+    <table cellspacing="0" width="470"
+        cellpadding="0" border="0">
+        <tr>
+            <td style="padding: 0 12px 0 0;vertical-align: top;" valign="top" width="85"> <a target="_blank"
+                    href="#"><img alt="Email Signature"
+                        width="85"
+                        style="width:85px;moz-border-radius:50%;khtml-border-radius:50%;o-border-radius:50%;webkit-border-radius:50%;ms-border-radius:50%;border-radius:50%;"
+                        src="res/img/logo.png" /></a>
+            </td>
+            <td style="font-size:1em;padding:0 0 0 12px;vertical-align: top;border-left: 1px solid #417505;"
+                valign="top">
+                <table cellspacing="0" cellpadding="0" border="0"
+                    style="line-height: 1.4;font-family:'Arial Black', Gadget, sans-serif;font-size:64%;color: #000001;">
+                    <tr>
+                        <td style="padding-bottom: 8px;"> <span
+                                style="font: 600 1.3em 'Arial Black', Gadget, sans-serif;color:#417505;">Ian
+                                Kipchirchir</span> </td>
+                    </tr>
+                    <tr>
+                        <td style="color:#000001;padding-bottom: 8px;">
+                            <div style="font-family:'Arial Black', Gadget, sans-serif;">Software Engineer | BitRev Labs
+                                | Engineering </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="font-family:'Arial Black', Gadget, sans-serif;"> <span
+                                    style="font-weight: 600;color:#417505;">M:&nbsp;</span> <a style="color: #000001;"
+                                    href="tel:+254723945591">+254723945591</a> </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="font-family:'Arial Black', Gadget, sans-serif;"> <span
+                                    style="font-weight: 600;font-size:1em;color:#417505;">E:&nbsp;</span> <a
+                                    href="mailto:kipchirchiriank@gmail.com" style="color: #000001;"
+                                    target="_blank">kipchirchiriank@gmail.com</a> </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 8px 0 0 0;">
+                            <div style="font-family:'Arial Black', Gadget, sans-serif;"> <a
+                                    href="https://www.iankipchirchir.com" style="color:#417505;"
+                                    target="_blank">www.iankipchirchir.com</a> </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding-top: 12px;">
+                            <table cellspacing="0" cellpadding="0" border="0">
+                                <tr>
+                                    <td style="font-family: Arial;padding: 0 5px 0 0;"><a target="_blank"
+                                            class="social_link" href="http://www.twitter.com/kipchirchiriank"><img
+                                                alt="Email Signature" style="width:22px;" width="22"
+                                                src="res/icons/facebook-original.svg"></a>
+                                    </td>
+                                    <td style="font-family: Arial;padding: 0 5px 0 0;"><a target="_blank"
+                                            class="social_link" href="http://www.twitter.com/KipchirchirIan"><img
+                                                alt="Email Signature" style="width:22px;" width="22"
+                                                src="res/icons/twitter-original.svg"></a>
+                                    </td>
+                                    <td style="font-family: Arial;padding: 0 5px 0 0;"><a target="_blank"
+                                            class="social_link" href="http://www.twitter.com/KipchirchirIan"><img
+                                                alt="Email Signature" style="width:22px;" width="22"
+                                                src="res/icons/linkedin-original.svg"></a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
